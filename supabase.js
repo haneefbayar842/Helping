ee8256af546a749a727070dbb103319b567a1ff1
haneefbayar842 <haneefbayar842@gmail.com>
2025-05-25 20:19:43 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -19,3 +19,12 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = 'https://yevkpqzirifkuvrcntxc.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9... (നിന്റെ anon key)'
 export const supabaseClient = createClient(supabaseUrl, supabaseKey)
+
+// supabase.js
+const { createClient } = supabase;
+
+const supabaseUrl = "https://yevkpqzirifkuvrcntxc.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldmtwcXppcmlma3V2cmNudHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNTk0MTcsImV4cCI6MjA2MzczNTQxN30.BRIJoMQqHeiOeU6fuCauHw-ybel8odMEuCZJEWIvAms";
+
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
